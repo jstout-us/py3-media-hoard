b5da9fbbac1b89a58fecd641f0aa9a6d75fa5350
Justin Stout <jstout-us@jstout.us>
2019-12-31 14:59:47 -0800

    add feed parse and normailization

---
+++ media_hoard/publisher.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+"""Module Media Hoard.publisher"""
+
+import feedparser
+
+from dateutil import parser
+
+
+def _normalize_item(entry):
+    """Normailize feed entry data.
+
+    Args:
+        entry(dict):    Entry dictionary from feedparser
+
+    Returns:
+        item(dict):     Normalized item data.
+    """
+    item = {
+        'title': entry['title'],
+        'subtitle': entry['subtitle'],
+        'author': entry['author'],
+        'duration': int(entry['itunes_duration']),
+        'time_published': parser.parse(entry['published']),
+        'guid': entry['id'],
+        'guid_is_uri': entry['guidislink'],
+        'uri_src': entry['links'][0]['href'],
+        'file_type': entry['links'][0]['type'],
+        }
+
+    return item
+
+
+def get_feed(src):
+    """Retreive and parse channel feed.
+
+    Args:
+        src(str):   url or file path to channel feed file.
+
+    Returns:
+        channel(dict):  The channel information dictionary
+        items(list):    List of channel item dictionaries
+    """
+    feed = feedparser.parse(src)
+
+    items = [_normalize_item(x) for x in feed.pop('entries')]
+
+    publisher = '{} ({})'.format(feed['feed']['publisher_detail']['name'],
+                                 feed['feed']['publisher_detail']['email'])
+
+    channel = {
+        'title': feed['feed']['title'],
+        'subtitle': feed['feed']['subtitle'],
+        'summary': feed['feed']['summary'],
+        'author': feed['feed']['author'],
+        'publisher': publisher,
+        'language': feed['feed']['language'],
+        'uri_site': feed['feed']['link'],
+        'uri_feed': src,
+        'uri_image': feed['feed']['image']['href'],
+        }
+
+    return (channel, items)
---
+++ tests/integration/test_int_subscribe.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+"""Test module doc string."""
+from datetime import datetime
+
+import pytest
+
+from dateutil import tz
+
+import media_hoard
+
+from media_hoard import publisher
+
+
+@pytest.fixture
+def fix_uri_rss():
+    return 'tests/_fixtures/feeds/behind_the_bastards.rss'
+
+
+@pytest.fixture
+def fix_channel_data(fix_uri_rss):
+    summary = "<p>There’s a reason the History Channel has produced hundreds of documentaries about Hitler but only a few about Dwight D. Eisenhower. Bad guys (and gals) are eternally fascinating. Behind the Bastards dives in past the Cliffs Notes of the worst humans in history and exposes the bizarre realities of their lives. Listeners will learn about the young adult novels that helped Hitler form his monstrous ideology, the founder of Blackwater’s insane quest to build his own Air Force, the bizarre lives of the sons and daughters of dictators and Saddam Hussein’s side career as a trashy romance novelist.</p>"
+    uri_icon = "https://megaphone-prod.s3.amazonaws.com/podcasts/22c36480-3778-11e8-806d-cfb84f1d0648/image/uploads_2F1547069138516-4edafwuejvc-14cc4c048140986ac2bacd6b0e0f022d_2FBehindTheBastards-Logo-iHR-FINAL-3000x3000.jpg"
+
+    fixture = {
+        'title': "Behind the Bastards",
+        'subtitle': "Everything you don't know about History's greatest monsters",
+        'author': "iHeartRadio",
+        'language': "en",
+        'publisher': "iHeartRadio (applepodcast@howstuffworks.com)",
+        'summary': summary,
+        'uri_site': 'https://www.behindthebastards.com/',
+        'uri_feed': fix_uri_rss,
+        'uri_image': uri_icon,
+        }
+
+    return fixture
+
+
+@pytest.fixture
+def fix_item_data():
+    fixture = {
+        'title': "Part One; The Idiot Who Made, And Destroyed, WeWork",
+        'subtitle': "",
+        'author': "iHeartRadio",
+        'duration': 5171,
+        'time_published': datetime(2019, 12, 17, 11, 0, tzinfo=tz.UTC),
+        'guid': "79f9cf16-a33b-11e9-949e-eb7c74e06cee",
+        'guid_is_uri': False,
+        'uri_src': 'https://www.podtrac.com/pts/redirect.mp3/chtbl.com/track/5899E/traffic.megaphone.fm/HSW7013309724.mp3',
+        'file_type': 'audio/mpeg',
+        }
+
+    return fixture
+
+
+def test_get_feed(fix_uri_rss, fix_channel_data, fix_item_data):
+    channel, items = publisher.get_feed(fix_uri_rss)
+
+    assert fix_channel_data == channel
+    assert fix_item_data == items[0]
+
+
+# def test_subscribe_rss():
+#     fix_rss = 'tests/_fixtures/feeds/behind_the_bastards.rss'
+#     expected_title = 'Behind the Bastards'
+#     expected_new_items = 173
+
+#     title, new_items = media_hoard.subscribe(fix_rss)
+
+#     assert expected_title == title
+#     assert expected_new_items == new_items
