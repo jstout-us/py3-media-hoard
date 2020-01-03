67eed45622d2de8c224860719ea6f3a12fc2ae3a
Justin Stout <jstout-us@jstout.us>
2020-01-03 12:49:55 -0800

    update env vars for test

--- tests/unit/test_unit_config.py
+++ tests/unit/test_unit_config.py
@@ -10,13 +10,14 @@ import pytest
 from media_hoard import config
 
 
+@mock.patch.dict(os.environ,{'HOME': '/home/user'})
 def test_get_data_root_default():
     expected = '/home/user/.local/share/jstout-us/media-hoard'
 
     assert expected == config.get_data_root()
 
 
-@mock.patch.dict(os.environ,{'MH_DATA_ROOT': '/alt/path'})
+@mock.patch.dict(os.environ,{'HOME': '/home/user', 'MH_DATA_ROOT': '/alt/path'})
 def test_get_data_root_override():
     expected = '/alt/path'
 
